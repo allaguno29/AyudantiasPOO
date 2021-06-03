fb3176205173e505fdec3a8f4534924f32daeaeb
alinacarpio15 <agcarpio@espol.edu.ec>
2021-06-03 15:14:43 -0500

    catalogo

--- TallerUnidad1P4/src/repraciones/CatalogoComponente.java
+++ TallerUnidad1P4/src/repraciones/CatalogoComponente.java
@@ -10,5 +10,13 @@ package repraciones;
  * @author rocio
  */
 public class CatalogoComponente {
-    
+    private Componente[] componentes;
+
+    public CatalogoComponente(){
+      componentes = new Componente[10]; 
+      componentes[0]=new Componente("bateria celular",100.0);
+      componentes[1]=new Componente("bateria computador",200.0);
+      componentes[2]=new Componente("display celular",150.0);
+      componentes[3]=new Componente("placa madre",300.0);
+    }
 }
